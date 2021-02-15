5444910d9540edab17bc9de935a52cdbe1db3a54
Ivan Jacome <j.kome@hotmail.com>
2021-02-14 19:13:02 -0600

    Add files via upload

--- ejercnummenor1.cpp
+++ ejercnummenor1.cpp
@@ -2,18 +2,22 @@
 
 
 #include <iostream>
-
 #include <stdlib.h>
+#include <fstream>
 
 using namespace std;
 
 int main() {
 
-  int N, menor, numero;
+  int N ;
+
 
   cout<<"Escoge entre 2 a 5 digitos ?: ";
 
   cin>>N;
+  int numero ;
+  int menor ;
+  int num [N];
 
 
 
@@ -21,27 +25,52 @@ int main() {
 
   {
 
-    cout<<"Ingrese un numero: ";
+    cout<<"Ingrese un numero : " ;
 
     cin>>numero;
+    num[i] = numero;
 
     if ( i == 0 )
 
     {
 
-      menor = numero;
+      menor  = numero  ;
 
-    }else if ( numero < menor )
+    }else if ( numero   < menor  )
 
       {
 
-        menor = numero;
+        menor  = numero  ;
 
       }
 
+
   }
 
-  cout<<"\nEl menor ingresado es: "<<menor<<endl;
+cout<<"\nLos numeros ingresados son : "<<endl;
+for (int j = 0 ; j < N; j++){
+        cout<< num [j]<<endl;
+
+}
+  cout<<"\nEl menor ingresado es: "<< menor  <<endl;
+
+
+
+  ofstream file;
+  file.open ("resultC.txt");
+  if (file.is_open())
+  {
+      file<<"\nLos numeros ingresados son : "<<endl;
+       for (int j = 0 ; j < N; j++){
+        file<< num [j]<<endl;
+
+}
+
+  file << "\nEl menor ingresado es: "<<menor;
+  file.close();
+  }
+  else cout << "No se puede abrir el archivo";
+
 
   return 0;
 
